dc578240f29c769a5f48647caf641b14d7a4bdaf
didenkoev93 <didenkoev93@gmail.com>
2022-11-16 23:01:03 +0700

    create tests in progress

--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -1,18 +1,17 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+
 class LibraryTest {
     /*create new books*/
-    Book one = new Book("Старик и море", "Хумингуэй");
-    Book two = new Book("Фиеста", "Хумингуэй");
-    Book three = new Book("Скотный двор", "Д. Оруэл");
-    Book four = new Book("Шантарам", "П.Д. Робертс");
-    Book five = new Book("Тревожные люди", "Ф. Бакман");
-    Book six = new Book("Вторая жизнь Уве", "Ф. Бакман");
-    Library library = new Library();
+    private final Book one = new Book("Старик и море", "Хумингуэй");
+    private final Book two = new Book("Фиеста", "Хумингуэй");
+    private final Book three = new Book("Скотный двор", "Д. Оруэл");
+    private final Book four = new Book("Шантарам", "П.Д. Робертс");
+    private final Book five = new Book("Тревожные люди", "Ф. Бакман");
+    private final Book six = new Book("Вторая жизнь Уве", "Ф. Бакман");
+    private final Library library = new Library();
 
-
-    @org.junit.jupiter.api.Test
-    void addBook() {
+    public void create() {
         /*add books in the Library*/
         library.addBook(one);
         library.addBook(two);
@@ -20,19 +19,42 @@ class LibraryTest {
         library.addBook(four);
         library.addBook(five);
         library.addBook(six);
-        assertEquals(6, library.getAllBooks().size());
+    }
+//    /*create new books*/
+//    Book one = new Book("Старик и море", "Хумингуэй");
+//    Book two = new Book("Фиеста", "Хумингуэй");
+//    Book three = new Book("Скотный двор", "Д. Оруэл");
+//    Book four = new Book("Шантарам", "П.Д. Робертс");
+//    Book five = new Book("Тревожные люди", "Ф. Бакман");
+//    Book six = new Book("Вторая жизнь Уве", "Ф. Бакман");
+//    Library library = new Library();
+//    /*add books in the Library*/
+
+    @org.junit.jupiter.api.Test
+    void addBook() {
+        create();
+        assertEquals(6, library.getCountOfAllBooks());
+        /*try to add the book library contains*/
+        assertFalse(library.addBook(four));
+        assertEquals(6, library.getCountOfAllBooks());
     }
 
     @org.junit.jupiter.api.Test
     void deleteBook() {
-        library.addBook(one);
-        library.addBook(two);
+        create();
         library.deleteBook("Старик и море");
-        assertEquals(1, library.getAllBooks().size());
+        assertEquals(5, library.getCountOfAllBooks());
+        /*try to delete the book library not contains*/
+        assertFalse(library.deleteBook("Крестный отец"));
+        assertEquals(5, library.getCountOfAllBooks());
     }
 
     @org.junit.jupiter.api.Test
     void findBook() {
+        create();
+        assertEquals(library.findBook("Скотный двор"), three);
+        /*try to find another the book*/
+        assertNull(library.findBook("1984"));
     }
 
     @org.junit.jupiter.api.Test
